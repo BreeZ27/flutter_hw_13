eaaeeb0a479fdad06120506b565c3147f0683872
Vladimir Kharchenco <82471986+BreeZ27@users.noreply.github.com>
2022-03-29 10:57:03 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,46 +4,20 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 enum CounterEvent { increase }
 
-class CounterBloc {
+class CounterBloc extends Bloc<CounterEvent, int> {
   int value = 0;
 
-  final _stateController = StreamController<int>();
-  final _eventController = StreamController<CounterEvent>();
-
-  Stream<int> get state => _stateController.stream;
-
-  Sink<CounterEvent> get action => _eventController.sink;
-
-  CounterBloc() {
-    _eventController.stream.listen(_handleEvent);
+  CounterBloc() : super(0) {
+    on<CounterEvent>((event, emit) => emit(state + 1));
   }
-
-  void dispose() {
-    _stateController.close();
-    _eventController.close();
-  }
-
-  void _handleEvent(CounterEvent action) async {
-    if (action == CounterEvent.increase) {
-      value++;
-    }
-    _stateController.add(value);
-  }
-}
-
-final counterProvider = StateNotifierProvider<Counter, int>((_) => Counter());
-
-class Counter extends StateNotifier<int> {
-  Counter() : super(0);
-
-  void increment() => state++;
 }
 
 class MyApp extends StatelessWidget {
@@ -56,27 +30,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BlocProvider<CounterBloc>(
+          create: (_) => CounterBloc(),
+          child: MyHomePage(title: 'Flutter Demo Home Page')),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late final CounterBloc bloc;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -85,74 +54,20 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            StreamBuilder<int>(
-                stream: bloc.state,
-                builder: (_, snapshot) {
-                  if (snapshot.hasData) {
-                    return Text(
-                      snapshot.data?.toString() ?? '',
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else {
-                    return const SizedBox.shrink();
-                  }
-                })
+            BlocBuilder<CounterBloc, int>(
+              builder: (_, state) => Text(
+                state.toString(),
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => bloc.action.add(CounterEvent.increase),
+        onPressed: () => context.read<CounterBloc>().add(CounterEvent.increase),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
   }
-
-  @override
-  void initState() {
-    super.initState();
-    bloc = CounterBloc();
-  }
-
-  @override
-  void dispose() {
-    bloc.dispose();
-    super.dispose();
-  }
 }
-
-// class MyHomePage extends ConsumerWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-//   final String title;
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final count = ref.watch(counterProvider);
-//     final provider = ref.watch(counterProvider.notifier);
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               count.toString(),
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: provider.increment,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
