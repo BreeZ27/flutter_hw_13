e733146b13263be3cc0386fcc98da6086fa3f254
Vladimir Kharchenco <82471986+BreeZ27@users.noreply.github.com>
2022-03-31 09:43:27 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,127 +3,50 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:redux/redux.dart';
-import 'package:flutter_redux/flutter_redux.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:redux/redux.dart';
+// import 'package:flutter_redux/flutter_redux.dart';
+import 'package:mobx/mobx.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'state.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class CounterIncremetAction {}
-
-@immutable
-class AppState {
-  final int value;
-
-  const AppState({this.value = 0});
-
-  factory AppState.initial() => const AppState();
-
-  AppState copyWith({int? value}) {
-    return AppState(value: value ?? this.value);
-  }
-
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is AppState &&
-          runtimeType == other.runtimeType &&
-          value == other.value;
-
-  @override
-  int get hashcode => value.hashCode;
-
-  @override
-  String toString() {
-    return 'AppState{value: $value}';
-  }
-}
-
-final incrementReducer = combineReducers<int>([
-  TypedReducer<int, CounterIncremetAction>(_increment),
-]);
-
-int _increment(int value, CounterIncremetAction action) => ++value;
-
-AppState appReducer(AppState state, action) {
-  return state.copyWith(value: incrementReducer(state.value, action));
-}
-
-class CounterConnector extends StatelessWidget {
-  const CounterConnector({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StoreConnector<AppState, _ViewModel>(
-      converter: _ViewModel.fromStore,
-      builder: (context, vm) {
-        return MyHomePage(
-          title: 'Flutter_HW_13',
-          value: vm.value,
-          increase: vm.onIncrease,
-        );
-      },
-    );
-  }
-}
-
-class _ViewModel {
-  final int value;
-  final VoidCallback onIncrease;
-
-  _ViewModel({required this.value, required this.onIncrease});
-
-  static _ViewModel fromStore(Store<AppState> store) => _ViewModel(
-        value: store.state.value,
-        onIncrease: () => store.dispatch(
-          CounterIncremetAction(),
-        ),
-      );
-}
-
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
-  final store = Store(appReducer, initialState: AppState.initial());
-
   @override
   Widget build(BuildContext context) {
-    return StoreProvider<AppState>(
-        store: store,
+    return Provider(
+        create: (context) => ExampleState(),
         child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: CounterConnector(),
+          home: const MyHomePage(title: 'Flutter_HW_13'),
         ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({
     Key? key,
     required this.title,
-    required this.value,
-    required this.increase,
   }) : super(key: key);
 
   final String title;
-  final int value;
-  final VoidCallback increase;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final state = Provider.of<ExampleState>(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -132,15 +55,16 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              widget.value.toString(),
-              style: Theme.of(context).textTheme.headline4,
-            )
+            Observer(
+                builder: (context) => Text(
+                      state.value.toString(),
+                      style: Theme.of(context).textTheme.headline4,
+                    ))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: widget.increase,
+        onPressed: state.increment,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
