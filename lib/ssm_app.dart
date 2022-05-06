3bfebe5574e8c581f55d07a7103abd0af1b944c7
Vladimir Kharchenco <82471986+BreeZ27@users.noreply.github.com>
2022-05-06 07:48:40 +0300

    Update ssm_app.dart

--- lib/ssm_app.dart
+++ lib/ssm_app.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:buisness/buisness.dart';
 import 'package:get_it/get_it.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
-}
-
-class Cart extends ChangeNotifier {
-  List cart = [];
-
-  int get totalPrice {
-    return cart.length * 16;
-  }
-
-  void add(item) {
-    cart.add(item);
-    notifyListeners();
-  }
-
-  void removeAll() {
-    cart.clear();
-    notifyListeners();
-  }
 }
 
 class _MyAppState extends State<MyApp> {
@@ -35,7 +17,6 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     _prodBlock = GetIt.I.get<ProductBlock>();
-    _prodBlock.add(const ProductBlockEvent.init());
   }
 
   @override
@@ -70,153 +51,88 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String toShow = 'No data';
-
-  final loading = const Scaffold(
-    body: Center(
-      child: CircularProgressIndicator(),
-    ),
-  );
-
-  void _prodUpdate(ProductBlockState state) {
-    return context.read<ProductBlock>().add(
-          const ProductBlockEvent.setProd(),
-        );
-  }
-
-  void _cartCleaner(ProductBlockState state) {
-    context.read<ProductBlock>().cleane();
-
-    return context.read<ProductBlock>().add(
-          const ProductBlockEvent.addProd(),
-        );
-  }
-
-  void _cartUpdate(ProductBlockState state, item) {
-    // context.read<ProductBlock>().productService.myCart.add(item);
+  void _cartUpdate(item) {
     context.read<ProductBlock>().addToCart(item);
+  }
 
-    return context.read<ProductBlock>().add(
-          const ProductBlockEvent.addProd(),
-        );
+  void _cartCleaner() {
+    context.read<ProductBlock>().cleane();
   }
 
   @override
   Widget build(BuildContext context) {
-    // var read = context.read<ProductBlock>().show();
-
-    return StreamBuilder<ProductBlockState>(
-      stream: context.read<ProductBlock>().state,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final state = snapshot.data;
-          return state!.map<Widget>(
-            loading: (_) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: Text(widget.title),
-                ),
-                body: const Center(
-                  child: Text('Loading'),
-                ),
-              );
-            },
-            loaded: (state) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: Text(widget.title),
-                ),
-                body: Center(
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              Container(
-                                alignment: Alignment.topLeft,
-                                padding: const EdgeInsets.all(8),
-                                child: Text(
-                                  'Полка товаров',
-                                  style: Theme.of(context).textTheme.headline6,
-                                ),
-                              ),
-                              // context.read<ProductBlock>().goods().map(
-                              //       (e) => ListTile(
-                              //         title: Text('Товар ${e.id.toString()}'),
-                              //         trailing: const Icon(Icons.add_box),
-                              //         onTap: () {
-                              //           _cartUpdate(state, e);
-                              //         },
-                              //       ),
-                              //     )
-                              ...state.prodData.array.values.map(
-                                (e) => ListTile(
-                                  title: Text('Товар ${e.id.toString()}'),
-                                  trailing: const Icon(Icons.add_box),
-                                  onTap: () {
-                                    _cartUpdate(state, e);
-                                  },
-                                ),
-                              ),
-                            ],
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Полка товаров',
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                    ),
+                    ...context.watch<ProductBlock>().goods().map(
+                          (e) => ListTile(
+                            title: Text(
+                              'Товар ${e.id}',
+                            ),
+                            trailing: const Icon(Icons.add_box),
+                            onTap: () => _cartUpdate(e),
                           ),
-                        ),
-                      ),
-                      const Divider(
-                        height: 20,
-                        color: Colors.black,
-                      ),
-                      Expanded(
-                          child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                'Корзина',
-                                style: Theme.of(context).textTheme.headline6,
+                        )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Корзина',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        ...context.watch<ProductBlock>().show().keys.map(
+                              (e) => ListTile(
+                                title: Text(
+                                  'Товар ${e.id}',
+                                ),
+                                trailing: Text(
+                                    'x ${context.watch<ProductBlock>().show()[e]}'),
                               ),
                             ),
-                            Column(
-                              children: [
-                                ...context.read<ProductBlock>().show().keys.map(
-                                      (e) => ListTile(
-                                        title: Text('Товар ${e.id}'),
-                                        trailing:
-                                            Text('x ${state.prodData.out[e]}'),
-                                      ),
-                                    )
-                                // ...state.prodData.out.keys.map(
-                                //   (e) => ListTile(
-                                //     title: Text('Товар ${e.id}'),
-                                //     trailing:
-                                //         Text('x ${state.prodData.out[e]}'),
-                                //   ),
-                                // ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ))
-                    ],
-                  ),
-                ),
-                floatingActionButton: FloatingActionButton(
-                  onPressed: () => _cartCleaner(state),
-                  // onPressed: () => _prodUpdate(state),
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.delete),
-                  elevation: 50,
-                ),
-              );
-            },
-          );
-        } else {
-          return loading;
-        }
-      },
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'К оплате: ${context.watch<ProductBlock>().sum()} у.е.',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _cartCleaner(),
+        child: const Icon(Icons.delete),
+      ),
     );
   }
 }
