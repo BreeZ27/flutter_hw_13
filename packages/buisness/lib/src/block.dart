43b223d9b52eebe23f282a453c035363c8699bb7
Vladimir Kharchenco <82471986+BreeZ27@users.noreply.github.com>
2022-05-18 07:39:00 +0300

    Update block.dart

--- packages/buisness/lib/src/block.dart
+++ packages/buisness/lib/src/block.dart
@@ -8,11 +8,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 GetIt getIt = GetIt.instance;
 
 enum ProductBlockEvent { clear, add, toCart }
-// abstract class ProductBlockEvent{}
-// class
 
-// class MyBLoc extends Bloc<ProductBlockEvent, Map<ProductData, int>> {
-// @injectable
 class MyBLoc extends Bloc<ProductBlockEvent, ProductBlock> {
   static int init = 0;
 
@@ -25,10 +21,6 @@ class MyBLoc extends Bloc<ProductBlockEvent, ProductBlock> {
   }
 
   MyBLoc() : super(ProductBlock()) {
-    // if (init == 0) {
-    //   state.createPrd(5);
-    //   init++;
-    // }
     on(
       (ProductBlockEvent event, emit) async {
         switch (event) {
@@ -56,73 +48,7 @@ class MyBLoc extends Bloc<ProductBlockEvent, ProductBlock> {
         }
       },
     );
-    // int init = 0;
-    // if (init == 0) {
-    // state.createPrd(5);
-    // productBlock.createPrd(5);
-    // emit(duplicate(state));
-    // init++;
-    // }
   }
-
-  // Stream<ProductBlock> toCart(ProductBlockEvent event, ProductData e) async* {
-  //   print('Stream toCart');
-  //   if (event == ProductBlockEvent.toCart) {
-  //     state.addToCart(e);
-  //     state.give();
-  //   }
-  //   yield state;
-  // }
-
-  // @override
-  // Stream<ProductBlock> mapEventToState(ProductBlockEvent event) async* {
-  //   switch (event) {
-  //     case ProductBlockEvent.add:
-  //       state.createPrd(5);
-  //       print('Stream: ProductBlockEvent.add');
-  //       // emit(state);
-  //       yield state;
-  //       break;
-  //     case ProductBlockEvent.clear:
-  //       state.cleaning();
-  //       print('Stream: ProductBlockEvent.clear');
-  //       yield state;
-  //       break;
-  //     case ProductBlockEvent.add_0:
-  //       state.addToCart(state.goods().keys[0]);
-  //       yield state;
-  //       break;
-  //     default:
-  //       print('Stream: default');
-  //   }
-  // }
-
-  // void _handleEvent(ProductBlockEvent action) async {
-  //   switch (action) {
-  //     case ProductBlockEvent.clear:
-  //       productBlock.cleaning();
-  //       print('_handleEvent clear');
-  //       break;
-  //     case ProductBlockEvent.add:
-  //       productBlock.createPrd(5);
-  //       print('_handleEvent add');
-  //       break;
-  //     default:
-  //       // productBlock.createPrd(5);
-  //       print('_handleEvent default');
-  //       break;
-  //   }
-  //   await Future.delayed(const Duration(seconds: 2));
-  // _stateContrl2.add(productBlock.goods());
-  // _stateContrl1.add(productBlock.show());
-  // }
-
-  // void _addProduct(ProductData item) async {
-  //   print('MyBLoc._addProduct($item)');
-  //   productBlock.addToCart(item);
-  //   await productBlock.give();
-  // _stateContrl1.add(productBlock.show());
-  // }
 }
 
 @injectable
